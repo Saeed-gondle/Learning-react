ce635c921846f08d77ad9ff7fd86ab71942c74ba
Muhammad Saeed <qasimgondle+github@gmail.com>
2025-10-05 22:09:43 +0500

    Refactor Menu and Pizza components for improved structure and readability

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -15,14 +15,42 @@ function Header() {
   );
 }
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
-      <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map(pizzaObj => {
-          return <Pizza key={pizzaObj.name} pizzaObj={pizzaObj} />;
-        })}
-      </ul>
+      <h2>Our menu</h2>
+
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+
+      {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      /> */}
     </main>
   );
 }
--- pizza-menu/src/Pizza.js
+++ pizza-menu/src/Pizza.js
@@ -1,11 +1,18 @@
 function Pizza({ pizzaObj }) {
-  return (
-    <li className="pizza">
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-      <h2>{pizzaObj.name}</h2>
+  return <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+    <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+    <div>
+      <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
-      <span>{+pizzaObj.price}</span>
-    </li>
-  );
+
+      {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+      <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
+    </div>
+  </li>;
 }
 export default Pizza;
