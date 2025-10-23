8ce22b01ea82cb73eede1f817259589f5b34214b
Muhammad Saeed <qasimgondle+github@gmail.com>
2025-10-23 12:26:26 +0500

    Add initial React app setup with essential files and configurations
    - Added logo512.png for app branding.
    - Created manifest.json for PWA support.
    - Added robots.txt for search engine directives.
    - Implemented App.css for styling the main application.
    - Developed App.js as the main component with a logo and link to React documentation.
    - Created App.test.js for basic testing of the App component.
    - Added index.css for global styles.
    - Set up index.js to render the App component.
    - Included logo.svg for the application logo.
    - Implemented reportWebVitals.js for performance tracking.
    - Set up testing environment with setupTests.js.

--- coding-challenges/src/index.js
+++ coding-challenges/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App-counter-clock';
+import ReactDOM from 'react-dom/client'; // Changed: Added /client
+// import App from './App-counter-clock';
+import App from './App-flash-cards.js';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,9 +1,125 @@
-
-import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [items, setItems] = useState([]);
+  
+  function handleDelete(id) {
+    setItems(items => items.filter(item => item.id !== id));
+  }
+  function handleToggle(id) {
+    setItems(items =>
+      items.map(item =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Logo />
+      <Form onAdd={setItems} />
+      <PackingList
+        items={items}
+        onDelete={handleDelete}
+        onToggle={handleToggle}
+      />
+      <Stats
+        stats={{
+          total: items.length,
+          packed: items.filter(item => item.packed).length,
+          percentage:
+            Math.floor(
+              (items.filter(item => item.packed).length / items.length) * 100
+            ) || 0,
+        }}
+      />
+    </div>
+  );
+}
+function Logo() {
+  return <h1 className="logo">🏝️ Far Away 🧳</h1>;
+}
+function Form({onAdd}) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  function handleSubmit(e) {
+    e.preventDefault();   
+    const item = {
+      id: Date.now(),
+      name: description,
+      quantity: quantity,
+      packed: false,
+    };
+    onAdd(items => [...items, item]);
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for trip?</h3>
+      <select name="quantity" className="select-item" value={quantity} onChange={e => setQuantity(e.target.value)}>
+        {Array(20)
+          .fill()
+          .map((_, i) => (
+            <option value={i + 1} key={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+      </select>
+      <input
+        type="text"
+        className="input-item"
+        name="item"
+        placeholder="Item..."
+        value={description}
+        onChange={e => {
+          setDescription(e.target.value);
+        }
+        }
+      />
+      <button className="btn btn-add" type="submit">Add</button>
+    </form>
+  );
+}
+function PackingList({items, onDelete, onToggle}) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map(item => (
+          <Item key={item.id} item={item} onDelete={onDelete} onToggle={onToggle} />
+        ))}
+      </ul>
+      <div className="actions">
+        <select>
+          <option value="input">Sort by Input</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by Packed</option>
+          <option value="unpacked">Sort by Unpacked</option>
+        </select>
+        <button className="btn btn-clear" onClick={() => onDelete(null)}>Clear List</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ item , onDelete, onToggle}) {
+  return (
+    <li
+      className="item"
+      style={item.packed ? { textDecoration: 'line-through' } : {}}
+    >
+      <input type="checkbox" checked={item.packed} onChange={() => onToggle(item.id)} />
+      <span className="item-name">
+        {item.quantity} {item.name}
+      </span>
+      <button className="btn btn-remove" onClick={() => onDelete(item.id)}>❌</button>
+    </li>
+  );
+}
+function Stats({stats: { total, packed ,percentage}}) {
+  return (
+    <footer className="stats">
+      💡 You have {total} items on your list, and you already packed {packed} ({ percentage}%).
+    </footer>
   );
 }
 export default App;
